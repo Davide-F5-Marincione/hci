ba6152d5b9d32b69ef2d9b184c315e9ecb3d3237
dario-loi <loi.1940849@studenti.uniroma1.it>
2023-05-15 20:32:55 +0200

    simulation changes

--- sim/routine.py
+++ sim/routine.py
@@ -1,18 +1,38 @@
 import random
 import pickle
 
-stops = ["Castro Laurenziano", "Viale Ippocrate",
-         "Piazzale Aldo Moro", "Policlinico",
-         "Verano", "Piazzale delle Provincie",
-         "Tiburtina", "Termini", "Porta Pia", "Castro Pretorio",
-         "Piazza dei Sanniti"]
+
+stops = [
+    "Castro Laurenziano",
+    "Viale Ippocrate",
+    "Piazzale Aldo Moro",
+    "Policlinico",
+    "Verano",
+    "Piazzale delle Provincie",
+    "Tiburtina",
+    "Termini",
+    "Porta Pia",
+    "Castro Pretorio",
+    "Piazza dei Sanniti",
+]
 
 lines = {
-    "1": (3, [("Castro Laurenziano", 60), ("Viale Ippocrate", 60), ("Policlinico", 120), ("Piazzale Aldo Moro", 240), ("Piazza dei Sanniti", 80), ("Verano", 60)])
+    "1": (
+        3,
+        [
+            ("Castro Laurenziano", 60),
+            ("Viale Ippocrate", 60),
+            ("Policlinico", 120),
+            ("Piazzale Aldo Moro", 240),
+            ("Piazza dei Sanniti", 80),
+            ("Verano", 60),
+        ],
+    )
 }
 
 delays = [0, 10, 30, 60, 120, 240]
 weights = [10, 20, 30, 40, 50, 60]
+
 
 def step(state):
     for busname, (this_line, last_place, time_left, overcrowded) in state.items():
@@ -22,18 +42,19 @@ def step(state):
             line = lines[this_line][1]
             for i, (place, _) in enumerate(line):
                 if place == last_place:
-                    next_place, time_left = line[(i+1) % len(line)]
+                    next_place, time_left = line[(i + 1) % len(line)]
 
                     time_left += random.choices(delays, weights, k=1)[0]
                     overcrowded = random.choices([True, False], [100, 50], k=1)[0]
                     break
-        
+
         state[busname] = (this_line, next_place, time_left, overcrowded)
+
 
 def random_setup(lines, names="sim/names.pickle", seed=None):
     if seed is not None:
         random.seed(seed)
-    
+
     with open(names, "+rb") as file:
         bus_names = pickle.load(file)
 
@@ -43,7 +64,7 @@ def random_setup(lines, names="sim/names.pickle", seed=None):
         n += v
 
     assert n <= len(bus_names)
-    
+
     random.shuffle(bus_names)
 
     i = 0
@@ -56,7 +77,6 @@ def random_setup(lines, names="sim/names.pickle", seed=None):
         random.shuffle(pos)
 
         for next_place, time_left in pos[:n]:
-
             time_left += random.choices(delays, weights, k=1)[0]
             overcrowded = random.choices([True, False], [100, 50], k=1)[0]
             buses[bus_names[i]] = (line, next_place, time_left, overcrowded)
@@ -65,10 +85,9 @@ def random_setup(lines, names="sim/names.pickle", seed=None):
     return buses
 
 
-    
+if __name__ == "__main__":
+    buses = random_setup(lines, seed=42)
 
-buses = random_setup(lines, seed=42)
-
-for t in range(0, 1000, 10):
-    step(buses)
-    print(buses)
+    for t in range(0, 1000, 10):
+        step(buses)
+        print(buses)
