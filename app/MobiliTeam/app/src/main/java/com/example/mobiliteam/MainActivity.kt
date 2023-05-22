2431ea4f2376be0d96da01d1c4276d76af5e528f
Davide Marincione <76567598+Davide-F5-Marincione@users.noreply.github.com>
2023-05-23 00:18:52 +0200

    name change

--- app/MobiliTeam/app/src/main/java/com/example/mobiliteam/MainActivity.kt
+++ app/MobiliTeam/app/src/main/java/com/example/mobiliteam/MainActivity.kt
@@ -9,7 +9,7 @@ import com.google.android.material.button.MaterialButton
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         val registerGoTo = findViewById<TextView>(R.id.registerGoTo)
         registerGoTo.setOnClickListener {
             val intent = Intent(this@MainActivity, RegisterActivity::class.java)
