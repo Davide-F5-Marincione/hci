88ae1cb9ad43e490c4418dae4ca5b6aeae207e7f
Davide Marincione <76567598+Davide-F5-Marincione@users.noreply.github.com>
2023-05-23 10:50:12 +0200

    better navigation

--- app/MobiliTeam/app/src/main/java/com/example/mobiliteam/PathFollowFragment.kt
+++ app/MobiliTeam/app/src/main/java/com/example/mobiliteam/PathFollowFragment.kt
@@ -2,10 +2,14 @@ package com.example.mobiliteam
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toolbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.example.mobiliteam.databinding.FragmentPathFollowBinding
 
@@ -37,6 +41,11 @@ class PathFollowFragment : Fragment() {
             val intent = Intent(activity, HomeActivity::class.java)
             startActivity(intent)
         }
+
+        (activity as TravelActivity).binding.toolbar.setNavigationOnClickListener(View.OnClickListener {
+            val intent = Intent(activity, HomeActivity::class.java)
+            startActivity(intent)
+        })
     }
 
     override fun onDestroyView() {
