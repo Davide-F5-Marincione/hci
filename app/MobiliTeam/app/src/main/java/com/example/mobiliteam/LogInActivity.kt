dca8f768129067e980f359a374b252b2ff4aefc8
MartinaDoku2001 <doku.1938629@studenti.uniroma1.it>
2023-05-23 11:23:41 +0200

    import http library and login implem (not working)

--- app/MobiliTeam/app/src/main/java/com/example/mobiliteam/LogInActivity.kt
+++ app/MobiliTeam/app/src/main/java/com/example/mobiliteam/LogInActivity.kt
@@ -1,12 +1,17 @@
 package com.example.mobiliteam
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.content.Intent
+import android.os.Bundle
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.button.MaterialButton
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+
 
 class LogInActivity : AppCompatActivity() {
+    var client: OkHttpClient = OkHttpClient()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -25,6 +30,35 @@ class LogInActivity : AppCompatActivity() {
 
         val logInButton = findViewById<MaterialButton>(R.id.logInButton)
         logInButton.setOnClickListener {
+            // take value from username field
+            val username = findViewById<TextView>(R.id.usernameInput).text.toString()
+            // perform a get request to the server to check if the username exists
+            // if it does, then go to the home page
+            // else, display an error message
+            val url ="http://127.0.0.1:5000/users/"+ username;// Replace with your API endpoint
+
+            val request: Request = Request.Builder()
+                .url(url)
+                .build()
+            try {
+                val response = client.newCall(request).execute()
+                val responseData = response.body!!.string()
+
+                //if there is a response, then the username exists
+                if (responseData != null) {
+                    // go to the home page
+                    val intent = Intent(this@LogInActivity, HomeActivity::class.java)
+                    startActivity(intent)
+                } else {
+                    // display an error message
+                    val errorMessage = findViewById<TextView>(R.id.errorMessage)
+                    errorMessage.text = "Username does not exist"
+                }
+            } catch (e: IOException) {
+                //display an error message
+                val errorMessage = findViewById<TextView>(R.id.errorMessage)
+                e.printStackTrace()
+            }
             val intent = Intent(this@LogInActivity, HomeActivity::class.java)
             startActivity(intent)
         }
