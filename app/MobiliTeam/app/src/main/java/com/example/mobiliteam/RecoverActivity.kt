ec6db203a0fdee5d937575081ec9040538103b07
Davide Marincione <76567598+Davide-F5-Marincione@users.noreply.github.com>
2023-05-23 12:29:27 +0200

    even more refactoring

--- app/MobiliTeam/app/src/main/java/com/example/mobiliteam/RecoverActivity.kt
+++ app/MobiliTeam/app/src/main/java/com/example/mobiliteam/RecoverActivity.kt
@@ -2,14 +2,13 @@ package com.example.mobiliteam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.content.Intent
 import android.widget.ImageView
 import com.google.android.material.button.MaterialButton
 
 class RecoverActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.recover_username)
+        setContentView(R.layout.activity_recover)
         val backButton = findViewById<ImageView>(R.id.backButton)
         backButton.setOnClickListener {
             finish()
